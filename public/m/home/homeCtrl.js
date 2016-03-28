ced596b389cbbaa290c2f690ccf44892e1b7483e
mnimmagari <madhumithareddy185@gmail.com>
2016-03-28 11:46:14 +0530

    Home header on scroll resize

--- public/m/home/homeCtrl.js
+++ public/m/home/homeCtrl.js
@@ -5,3 +5,16 @@ var app = angular.module('home');
 app.controller('homeCtrl', ['$scope', 'location', function ($scope, location) {
    location.get(angular.noop, angular.noop);
 }]);
+app.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+    	scope.navClass = 'big';
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset >= 0) {
+                 scope.navClass = 'small';
+             } else {
+                  scope.navClass = 'big';
+             }
+            scope.$apply();
+        });
+    };
+});
