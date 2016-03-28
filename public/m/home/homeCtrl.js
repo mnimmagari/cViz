1ef5159389bf8408a59606292566ccd0ff619e0f
mnimmagari <madhumithareddy185@gmail.com>
2016-03-28 21:15:37 +0530

    rough commit

--- public/m/home/homeCtrl.js
+++ public/m/home/homeCtrl.js
@@ -4,15 +4,19 @@ var app = angular.module('home');
 
 app.controller('homeCtrl', ['$scope', 'location', function ($scope, location) {
    location.get(angular.noop, angular.noop);
+  // $scope.data = false;
 }]);
 app.directive("scroll", function ($window) {
     return function(scope, element, attrs) {
-    	scope.navClass = 'big';
+    	// scope.navClass = 'big';
+        scope.data = false;
         angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 0) {
-                 scope.navClass = 'small';
+             if (this.pageYOffset >= 100) {
+                 // scope.navClass = 'small';
+                 scope.data = true;
              } else {
-                  scope.navClass = 'big';
+                  // scope.navClass = 'big';
+                  scope.data = false;
              }
             scope.$apply();
         });
