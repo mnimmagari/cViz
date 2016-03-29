fdfe4ece5300404378573b6bd6603f9d224cef37
mnimmagari <madhumithareddy185@gmail.com>
2016-03-29 10:56:52 +0530

    Home header scroll animation

--- public/m/home/homeCtrl.js
+++ public/m/home/homeCtrl.js
@@ -1,23 +1,18 @@
-
-
 var app = angular.module('home');
 
-app.controller('homeCtrl', ['$scope', 'location', function ($scope, location) {
-   location.get(angular.noop, angular.noop);
-  // $scope.data = false;
+app.controller('homeCtrl', ['$scope', 'location', function($scope, location) {
+    location.get(angular.noop, angular.noop);
+
 }]);
-app.directive("scroll", function ($window) {
+app.directive("scroll", function($window) {
     return function(scope, element, attrs) {
-    	// scope.navClass = 'big';
         scope.data = false;
         angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 100) {
-                 // scope.navClass = 'small';
-                 scope.data = true;
-             } else {
-                  // scope.navClass = 'big';
-                  scope.data = false;
-             }
+            if (this.pageYOffset >= 100) {
+                scope.data = true;
+            } else {
+                scope.data = false;
+            }
             scope.$apply();
         });
     };
